991357179740a31db7f9caa24b41eba86e7ab004
NargesBeck <60057086+NargesBeck@users.noreply.github.com>
2023-11-10 15:31:01 +0330

    Init HelloGame Models

---
+++ HelloGame/HelloGame/Models/Quest.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloGame.Models
+{
+    public class Quest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int XPRequirement { get; set; }
+    }
+}
---
+++ HelloGame/HelloGame/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloGame.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime JoinDate { get; set; }
+        public int XP { get; set; }
+        public ICollection<Quest> Quests { get; set; }
+    }
+}
